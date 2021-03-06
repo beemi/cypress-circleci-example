daeca9430deb2998e9bf470744c820fdd367e5ec
beemi <beemi.raja@gmail.com>
2021-03-06 11:40:13 +0000

    [ci skip] - renovate update

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -1,9 +1,9 @@
 context('Actions', () => {
     beforeEach(() => {
         cy.visit('http://automationpractice.com/index.php')
-    })
+    });
 
     it.only('should be on the page', () => {
         cy.url().should('include', "/index.php")
-    })
+    });
 })
